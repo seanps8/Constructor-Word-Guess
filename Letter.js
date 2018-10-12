c6c21fe3f17d7653e86e02705b16f183e96ac169
Blazer <sstubbs012@gmail.com>
2018-10-11 22:00:58 -0400

    first try

--- Letter.js
+++ Letter.js
@@ -1,6 +1,21 @@
 function Letter(letter) {
     this.letter = letter;
     this.guessed = false;
+    this.toString = function () {
+        if (this.guessed) {
+            console.log(this.letter);
+        } else {
+            console.log("_");
+        }
+    };
+    this.check = function (letter) {
+        if (letter === this.letter) {
+            this.guessed = true;
+        } else {
+            this.guessed = false;
+        }
+    }
+
 }
 
 
